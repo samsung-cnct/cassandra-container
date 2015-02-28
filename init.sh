8d54a8286ff5c66e533953359adffcdab6b6746b
Mikel Nelson <mikel.nelson@mtnsat.com>
2015-02-28 08:53:39 -0800

    set stomp address after all

--- init.sh
+++ init.sh
@@ -3,6 +3,12 @@
 # Accept listen_address
 # IP=`hostname --ip-address` # old way
 IP=${LISTEN_ADDRESS:-`hostname --ip-address`}
+#
+# NOTE: this ASSUMES the container name will be cass0 - cass9 
+# TODO: make more generic based on ports alone
+#
+# NOTE: last sed creates a CSV list
+#
 SEEDS=`env | grep CASS[0-9]_PORT_9042_TCP_ADDR | sed 's/CASS[0-9]_PORT_9042_TCP_ADDR=//g' | sed -e :a -e N -e 's/\n/,/' -e ta`
 
 if [ -z "$SEEDS" ]; then
@@ -31,7 +37,21 @@ if [[ $DC && $RACK ]]; then
 fi
 
 # Start Monitor Agent
-echo Starting Agent...
+#
+# NOTE: opscenter MUST be running first so we can pull the IP of the container from the env vars...
+#
+# NOTE: assumes opcenter container is named opscenter
+# TODO: make more generic based on ports alone
+#
+# NOTE: last sed creates a CSV list.. should never be needed here
+#
+STOMP=`env | grep OPSCENTER_PORT_61620_TCP_ADDR | sed 's/OPSCENTER_PORT_61620_TCP_ADDR=//g' | sed -e :a -e N -e 's/\n/,/' -e ta`
+DCONFIG=/var/lib/datastax-agent/conf
+if [ -n "$STOMP" ]; then
+    echo "stomp_interface: $STOMP" | sudo tee -a $DCONFIG/address.yaml
+fi
+
+echo "Starting Datastax Agent... stomp: $STOMP"
 sudo service datastax-agent start
 
 # Start Cassandra
