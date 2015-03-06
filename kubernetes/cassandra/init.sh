af68f075f084fddde9f85d8d3ffb66b5097b3198
Mikel Nelson <mikel.nelson@mtnsat.com>
2015-03-06 11:31:45 -0800

    kub init

--- kubernetes/cassandra/init.sh
+++ kubernetes/cassandra/init.sh
@@ -9,7 +9,8 @@ IP=${LISTEN_ADDRESS:-`hostname --ip-address`}
 #
 # NOTE: last sed creates a CSV list
 #
-SEEDS=`env | grep CASS[0-9]_PORT_9042_TCP_ADDR | sed 's/CASS[0-9]_PORT_9042_TCP_ADDR=//g' | sed -e :a -e N -e 's/\n/,/' -e ta`
+#SEEDS=`env | grep CASS[0-9]_PORT_9042_TCP_ADDR | sed 's/CASS[0-9]_PORT_9042_TCP_ADDR=//g' | sed -e :a -e N -e 's/\n/,/' -e ta`
+SEEDS=`env | grep ^C[a-z,A-Z,0-9]*_PORT_9042_TCP_ADDR | cut -d "=" -f 2  | sed -e :a -e N -e 's/\n/,/' -e ta`
 
 if [ -z "$SEEDS" ]; then
   SEEDS=$IP
@@ -50,6 +51,11 @@ DCONFIG=/var/lib/datastax-agent/conf
 if [ -n "$STOMP" ]; then
     echo "stomp_interface: $STOMP" | sudo tee -a $DCONFIG/address.yaml
 fi
+
+#
+# for custom kubernetes seed 
+#
+export CLASSPATH=/kubernetes-cassandra.jar
 
 echo "Starting Datastax Agent... stomp: $STOMP"
 sudo service datastax-agent start
