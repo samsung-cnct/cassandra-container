15ba6f82efe7c826cace62cc125d501587ee92a1
Mikel Nelson <mikel.nelson@mtnsat.com>
2015-03-14 19:57:49 -0700

    build better

--- kubernetes/cassandra/init.sh
+++ kubernetes/cassandra/init.sh
@@ -1,41 +1,10 @@
-#!/usr/bin/env bash
-
-# Accept listen_address
-# IP=`hostname --ip-address` # old way
-IP=${LISTEN_ADDRESS:-`hostname --ip-address`}
-#
-# NOTE: this ASSUMES the container name will be cass0 - cass9 
-# TODO: make more generic based on ports alone
-#
-# NOTE: last sed creates a CSV list
-#
-#SEEDS=`env | grep CASS[0-9]_PORT_9042_TCP_ADDR | sed 's/CASS[0-9]_PORT_9042_TCP_ADDR=//g' | sed -e :a -e N -e 's/\n/,/' -e ta`
-SEEDS=`env | grep ^C[a-z,A-Z,0-9]*_PORT_9042_TCP_ADDR | cut -d "=" -f 2  | sed -e :a -e N -e 's/\n/,/' -e ta`
-
-if [ -z "$SEEDS" ]; then
-  SEEDS=$IP
-fi
-
-echo "Listening on: "$IP
-echo "Found seeds: "$SEEDS
+#!/bin/bash
 
 # Setup Cassandra
 CONFIG=/etc/cassandra/
-sed -i -e "s/^listen_address.*/listen_address: $IP/"            $CONFIG/cassandra.yaml
-sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"              $CONFIG/cassandra.yaml
-sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/"       $CONFIG/cassandra.yaml
-sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" $CONFIG/cassandra-env.sh
-
-#
-# from other script
-#
-if [[ $SNITCH ]]; then
-  sed -i -e "s/endpoint_snitch: SimpleSnitch/endpoint_snitch: $SNITCH/" $CONFIG/cassandra.yaml
-fi
-if [[ $DC && $RACK ]]; then
-  echo "dc=$DC" > $CONFIG/cassandra-rackdc.properties
-  echo "rack=$RACK" >> $CONFIG/cassandra-rackdc.properties
-fi
+IP=${LISTEN_ADDRESS:-`hostname --ip-address`}
+#perl -pi -e "s/%%ip%%/$(hostname -I)/g" /etc/cassandra/cassandra.yaml
+perl -pi -e "s/%%ip%%/$IP/g" $CONFIG/cassandra.yaml
 
 # Start Monitor Agent
 #
